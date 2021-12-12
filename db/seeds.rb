792642127eb46ff30d75440410e01dcab2cebfe8
Dharmendra Solanki <dharmendra.solanki@emorphis.in>
2021-12-12 21:55:51 +0530

    Added seed file for create a admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-['Speed Post', 'Regular'].each do |name|
-	ServiceType.create(name: name)
-end
+# ['Speed Post', 'Regular'].each do |name|
+# 	ServiceType.create(name: name)
+# end
 
 
-['sender', 'receiver'].each do |name|
-	user = User.new(name: name, email: "#{name}@gmail.com")
+# ['sender', 'receiver'].each do |name|
+# 	user = User.new(name: name, email: "#{name}@gmail.com")
+# 	user.build_address(address_line_one: "Test Street #{name}", city: "New Delhi",
+# 		               state: "New Delhi", country: "India", pincode: 110111,
+# 		               mobile_number: '9999999999')
+# 	user.save
+# end
+
+
+['admin'].each do |name|
+	user = User.new(name: name, email: "#{name}@gmail.com", role: 'Admin', encrypted_password: BCrypt::Password.create("qwerty") )
 	user.build_address(address_line_one: "Test Street #{name}", city: "New Delhi",
 		               state: "New Delhi", country: "India", pincode: 110111,
 		               mobile_number: '9999999999')
